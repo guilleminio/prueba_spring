44db57a7cebc0e3097713208ae8f4238e44cb32d
Guille! <91991551+guilleminio@users.noreply.github.com>
2023-03-06 13:05:40 -0300

    Agregado de la notación @Repository

--- src/main/java/com/guilleminio/gestionusuarios/repository/IUserRepository.java
+++ src/main/java/com/guilleminio/gestionusuarios/repository/IUserRepository.java
@@ -2,6 +2,8 @@ package com.guilleminio.gestionusuarios.repository;
 
 import com.guilleminio.gestionusuarios.models.UserDTO;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IUserRepository extends CrudRepository<UserDTO,Integer> {
 }
